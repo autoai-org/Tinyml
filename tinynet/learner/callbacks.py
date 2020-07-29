6dec04d02a4c812e8ff0ab91a90e7a9b7d25f7ac
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-07-30 00:50:32 +0200

    minor bug fix

--- tinynet/learner/callbacks.py
+++ tinynet/learner/callbacks.py
@@ -6,8 +6,8 @@ def evaluate_classification_accuracy(model, epoch, data, label):
     batch_size = 1
     for i in range(0,len(data), batch_size):
         y_predict = model.predict(data[i:i+batch_size])
-        correct = np.argmax(y_predict, axis=1) == label[i:i+batch_size]
-    print("Epoch: {}, Acc: {:.4f}%".format(epoch, 100 * correct/len(data)))
+        correct += np.argmax(y_predict, axis=1) == label[i:i+batch_size]
+    print("Epoch: {}, Acc: {}%".format(epoch, 100 * correct/len(data)))
 
 def save_model(model, epoch,data,label):
     model.export('./model-{}.tnn'.format(epoch))
