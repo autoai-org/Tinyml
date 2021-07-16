ab24fa1a6b93f470e26df6519f05da08cd241330
Xiaozhe Yao <askxzyao@gmail.com>
2021-07-16 15:25:22 +0200

    add polynomial regression in functions

---
+++ tinyml/functional/polynomial_regression.py
@@ -0,0 +1,18 @@
+from tinyml.core import Backend as np
+
+def polynomial_regression(X, y, degree=1):
+    N = len(X)
+    matX = [np.ones(N)]
+    for power in range(degree):
+        matX.append(np.power(X, power+1))
+    matX = np.column_stack(matX)
+    A = np.linalg.pinv(matX.T @ matX)
+    D = A @ matX.T
+    return D @ y
+
+def polynomial_regression_predict(degree, coeff, X):
+    y_pred = 0
+    for power in range(degree):
+        y_pred += coeff[power+1] * np.power(X, power+1)
+    y_pred + coeff[0]
+    return y_pred
